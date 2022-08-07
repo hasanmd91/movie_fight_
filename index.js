dedc4851ee90cbd40f20ef5b0a2f091731ae5dae
hasanmd91 <hasanmd1991@hotmail.com>
2022-08-07 23:43:15 +0300

    fetch ready

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+const fetchData = async (searhTerm) => {
+  const response = await axios.get("http://www.omdbapi.com/", {
+    params: {
+      apikey: "333e8f3",
+      s: searhTerm,
+    },
+  });
 
+  console.log(response.data);
+};
+
+const input = document.querySelector("input");
+input.addEventListener("input", (e) => {
+  fetchData(e.target.value);
+});
