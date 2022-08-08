14842d3d76275fa3374ed9daf95c48ae1773636d
hasanmd91 <hasanmd1991@hotmail.com>
2022-08-08 22:32:25 +0300

    itertaion complete

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const fetchData = async (searhTerm) => {
       s: searhTerm,
     },
   });
+
+  return response.data.Search;
 };
 const input = document.querySelector("input");
 
-const onInput = (e) => {
-  fetchData(e.target.value);
+const onInput = async (e) => {
+  const movies = await fetchData(e.target.value);
+  console.log(movies);
+  for (let movie of movies) {
+    const div = document.createElement("div");
+    div.innerHTML = ` <img  src="${movie.Poster}"/> 
+    <h1> ${movie.Title}`;
+
+    document.getElementById("target").appendChild(div);
+  }
 };
 
 input.addEventListener("input", debounce(onInput, 1000));
