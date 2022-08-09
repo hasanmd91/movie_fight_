6806d5f8e8bc9fc05414daf695172a75745ead0d
hasanmd91 <hasanmd1991@hotmail.com>
2022-08-09 12:31:28 +0300

    fetch ready

--- index.js
+++ index.js
@@ -6,20 +6,39 @@ const fetchData = async (searhTerm) => {
     },
   });
 
+  if (response.data.Error) {
+    return [];
+  }
   return response.data.Search;
 };
+
+const root = document.querySelector(".autocomplete");
+root.innerHTML = `
+<label> <b> Search for a Movie <b></label>
+<input class="input"/> 
+  <div class="dropdown"> 
+    <div class="dropdown-menu" > 
+    <div class="dropdown-content results"> </div> 
+    </div>  
+  </div> 
+`;
 const input = document.querySelector("input");
+const dropdown = document.querySelector(".dropdown");
+const resultsWrapper = document.querySelector(".results");
 
 const onInput = async (e) => {
   const movies = await fetchData(e.target.value);
-  console.log(movies);
+  resultsWrapper.innerHTML = "";
+  dropdown.classList.add("is-active");
   for (let movie of movies) {
-    const div = document.createElement("div");
-    div.innerHTML = ` <img  src="${movie.Poster}"/> 
-    <h1> ${movie.Title}`;
+    const option = document.createElement("a");
+    option.classList.add("dropdown-item");
+    option.innerHTML = ` 
+    <img src="${movie.Poster}"/> 
+     ${movie.Title}`;
 
-    document.getElementById("target").appendChild(div);
+    resultsWrapper.appendChild(option);
   }
 };
 
-input.addEventListener("input", debounce(onInput, 1000));
+input.addEventListener("input", debounce(onInput, 500));
